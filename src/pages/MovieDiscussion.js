fdd601e7b9b9b8b3719f6cdf2910e804f61d0289
zamroni <forheron@gmail.com>
2022-11-17 17:44:22 +0700

    create discussion page

--- src/pages/MovieDiscussion.js
+++ src/pages/MovieDiscussion.js
@@ -1,8 +1,101 @@
 import React from 'react'
+import Avatar from 'react-avatar'
+import { Link } from 'react-router-dom'
+
+import discussions from "../assets/data/discussions.json"
 
 const MovieDiscussion = () => {
+
+  const statusses = (status) => {
+    let color = "";
+    switch (status) {
+      case 'open':
+        color = 'green'
+        break;
+      case 'closed':
+        color = 'red'
+        break;
+      case 'pending':
+        color = 'yellow'
+        break;
+    }
+
+   return (
+      <span class={` bg-${color}-200 text-${color}-600 py-1 px-3 rounded-full text-xs capitalize`}>{status}</span>
+   )
+  }
+
   return (
-    <div>MovieDiscussion</div>
+    <>
+      <div className='w-full lg:w-4/6 mx-auto'>
+      <div class="overflow-x-auto">
+        <div class="min-w-screen bg-white flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+            <div class="w-full py-4 lg:w-5/6 px-4">
+              <div className='flex flex-col md:flex-row mb-3 justify-between'>
+                <span className='text-2xl'>Discuss Parasite</span>
+                <div className='justify-center items-center text-center'>
+                  <button className="border border-blue-500 bg-sky-600 hover:bg-sky-700  text-white block rounded-lg font-bold py-2 w-full  px-4 ">
+                    <span>New Discussion</span>
+                  </button>
+                </div>
+              </div>
+                <div class="bg-white shadow-md rounded my-2 mt-8">
+                    <table class="min-w-max2 w-full table-auto">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                <th class="py-3 px-6 text-left w-5/12">Subject</th>
+                                <th class="py-3 px-6 text-center w-2/12">Users</th>
+                                <th class="py-3 px-6 text-center w-1/12">Status</th>
+                                <th class="py-3 px-6 text-center w-1/12">Replies</th>
+                                <th class="py-3 px-6 text-center w-3/12">Last Reply</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-gray-600 text-sm font-light w-full overflow-x-auto">
+                          { discussions.map((discuss, id) => (
+                            <tr class="border-b border-gray-200 hover:bg-gray-100 w-full"  key={id}>
+                                <td class="py-3 px-3 text-left whitespace-nowrap2 w-5/12">
+                                    <div class="flex items-center">
+                                        <div class="mr-2">
+                                          <Avatar name={discuss.name} size={40} round={true} />
+                                        </div>
+                                        <div className='leading-tight'>
+                                          <span class="font-medium">{discuss.subject}</span>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-left w-2/12">
+                                <div class="flex items-center justify-center">
+                                        <img class="w-6 h-6 rounded-full border-gray-200 border transform hover:scale-125" src="https://randomuser.me/api/portraits/men/1.jpg"/>
+                                        <img class="w-6 h-6 rounded-full border-gray-200 border -m-1 transform hover:scale-125" src="https://randomuser.me/api/portraits/women/2.jpg"/>
+                                        <img class="w-6 h-6 rounded-full border-gray-200 border -m-1 transform hover:scale-125" src="https://randomuser.me/api/portraits/men/3.jpg"/>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center w-1/12">
+                                  {statusses(discuss.status)}
+                                </td>
+                                <td class="py-3 px-6 text-center w-1/12">
+                                    <span>{discuss.reply}</span>
+                                </td>
+                                <td class="py-3 px-6 text-right w-3/12">
+                                    <div class="flex flex-col item-center justify-center">
+                                        <span>{discuss.created_at}</span>
+                                        <div>
+                                          by
+                                          <span className='ml-2 text-blue-300'>raze464</span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                          ))}
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/MovieMedia.js
+++ src/pages/MovieMedia.js
@@ -1,9 +1,152 @@
-import React from 'react'
+import React from "react";
 
 const MovieMedia = () => {
   return (
-    <div>MovieMedia</div>
-  )
-}
+    <>
+      <section class="text-gray-600 body-font">
+        <div class=" px-5 py-10 mx-auto">
+          <div class=" container flex flex-col text-center w-full mb-10 mx-auto">
+            <h1 class="md:text-3xl text-lg font-medium title-font mb-4 text-gray-900">
+              Gambar Latar
+            </h1>
+            <p class="lg:w-2/3 mx-auto leading-tight md:leading-snug text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep jianbing selfies heirloom.
+            </p>
+          </div>
+          <div class="flex flex-wrap -m-4">
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/600x360"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Shooting Stars
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/601x361"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    The Catalyzer
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/603x363"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    The 400 Blows
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/602x362"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Neptune
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/605x365"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Holden Caulfield
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="md:w-1/3 sm:w-1/2 p-2 md:p-4">
+              <div class="flex relative">
+                <img
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full object-cover object-center"
+                  src="https://dummyimage.com/606x366"
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                    THE SUBTITLE
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Alper Kamu
+                  </h1>
+                  <p class="leading-relaxed">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default MovieMedia
+export default MovieMedia;
